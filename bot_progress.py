6eae605c148b16e91e137833a9592db3b63fcbb8
dulledlove <108100952+dulledlove@users.noreply.github.com>
2022-06-23 17:42:33 -0500

    ♡
    €

---
+++ bot_progress.py
@@ -0,0 +1,11 @@
+setTimeout( (30) => {
+    let demo = ["a", "b", "c", "d"];
+    let Picked = demo[Math.floor(Math.random() * demo.length)]);
+    //I already explained the above part
+    Channel.send(Picked)
+        .then( msg => {
+              @everyone rblxwild rain! join up.
+              <a href = "https://www.rblxwild.com"> 
+            //If you wanna do anything like adding reactions
+        }
+},1000);
